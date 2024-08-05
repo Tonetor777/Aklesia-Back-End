e31f595c622c36d10fe932b9c929c658e9e0fb12
robel <robelwon75@gmail.com>
2024-08-05 12:11:23 +0300

    missed a slash on the token url

--- userAuth/urls.py
+++ userAuth/urls.py
@@ -3,6 +3,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 urlpatterns = [
-    path('user/token', TokenObtainPairView.as_view()),
+    path('user/token/', TokenObtainPairView.as_view()),
     path('user/token/refresh/', TokenRefreshView.as_view())
 ]
